7d167c679a6dc32a2c7c837dd57c762f9732a047
dshoulders <dshoulders@outlook.com>
2017-11-02 11:20:12 +0000

    CORE-2864 - Convert image component to a class

--- js/components/image.tsx
+++ js/components/image.tsx
@@ -2,41 +2,40 @@
 
 declare var manywho: any;
 
-(function (manywho) {
+interface IImageState {
+}
 
-    const image = React.createClass({
+// Cannot use 'Image' for class name as it clashes with the browser native window.Image class
+class MWImage extends React.Component<IComponentProps, IImageState> {
 
-        render: function () {
+    render () {
 
-            const classes = manywho.styling.getClasses(this.props.parentId, this.props.id, 'image', this.props.flowKey);
-            const model = manywho.model.getComponent(this.props.id, this.props.flowKey);
-            const outcomes = manywho.model.getOutcomes(this.props.id, this.props.flowKey);
-            const label = model.label;
+        const classes = manywho.styling.getClasses(this.props.parentId, this.props.id, 'image', this.props.flowKey);
+        const model = manywho.model.getComponent(this.props.id, this.props.flowKey);
+        const outcomes = manywho.model.getOutcomes(this.props.id, this.props.flowKey);
+        const label = model.label;
 
-            const outcomeButtons = outcomes && outcomes.map(function (outcome) {
-                return React.createElement(manywho.component.getByName('outcome'), { id: outcome.id, flowKey: this.props.flowKey });
-            }, this);
+        const outcomeButtons = outcomes && outcomes.map(function (outcome) {
+            return React.createElement(manywho.component.getByName('outcome'), { id: outcome.id, flowKey: this.props.flowKey });
+        }, this);
 
-            if (model.isVisible !== true) {
+        if (model.isVisible !== true) {
 
-                classes.push('hidden');
-
-            }
-
-            return (
-                <div className={classes.join(' ')} id={this.props.id}>
-                    {
-                        !manywho.utils.isNullOrWhitespace(label) ? <label className={'img-label'}>{label}</label> : null
-                    }
-                    <img className="img-responsive" src={model.imageUri} alt={model.developerName} id={this.props.id} />
-                    {outcomeButtons}
-                </div>
-            );
+            classes.push('hidden');
 
         }
 
-    });
+        return (
+            <div className={classes.join(' ')} id={this.props.id}>
+                {
+                    !manywho.utils.isNullOrWhitespace(label) ? <label className={'img-label'}>{label}</label> : null
+                }
+                <img className="img-responsive" src={model.imageUri} alt={model.developerName} id={this.props.id} />
+                {outcomeButtons}
+            </div>
+        );
 
-    manywho.component.register('image', image);
+    }
+}
 
-}(manywho));
+manywho.component.register('image', MWImage);
