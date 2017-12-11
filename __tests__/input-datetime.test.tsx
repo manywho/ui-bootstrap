55ef672ae7fe2c930d7d19b3c6f60421228358b5
jamesbrattBoomi <james_bratt@dell.com>
2017-12-11 15:37:13 +0000

    CORE-3646 - Implemented mock for datetimepicker plugin to enable further branch coverage for the datetime input component

--- __tests__/input-datetime.test.tsx
+++ __tests__/input-datetime.test.tsx
@@ -1,35 +1,51 @@
 import testUtils from '../test-utils';
 import * as React from 'react';
-import { mount } from 'enzyme';
+
+import { mount, shallow } from 'enzyme';
 
 import InputDateTime from '../js/components/input-datetime';
 
+const globalAny:any = global;
+
+const mockDatetimepicker = jest.fn((options) => {
+    return { on: jest.fn() };
+});
+
+globalAny['datetimepickerMock'] = mockDatetimepicker;
+
 jest.mock('jquery', () => {
-    return () => ({
-        data: () => ({
-            date: () => 'xxx',
-            destroy: () => {},
-        }),
-        datetimepicker: () => ({
-            on: () => {},
-        }),
+    return jest.fn(() => {
+        return {
+            datetimepicker: mockDatetimepicker,
+            data: () => ({
+                date: () => 'xxx',
+                destroy: () => {},
+            }),
+        };
     });
 });
 
 jest.mock('../js/lib/100-datetimepicker.js', () => 'xxx');
 
-
 describe('InputDateTime component behaviour', () => {
 
     let componentWrapper;
+    let model;
 
-    const globalAny:any = global;
+    function manyWhoMount(isShallow = false, useCurrent = false, dateTimeFormat = null) {
 
-    function manyWhoMount() {
+        model = {
+            attributes: {
+                dateTimeFormat,
+                useCurrent,
+                dateTimeLocale: 'en-us',
+            },
+        };
 
-        globalAny.window.manywho.model.getComponent = () => ({
-            attributes: {},
-        });
+        globalAny.window.manywho.model.getComponent = jest.fn(() => model),
+        globalAny.window.manywho['utils'] = {
+            isEqual: jest.fn(),
+        };
 
         globalAny.window.moment = () => ({
             utc() {
@@ -38,6 +54,9 @@ describe('InputDateTime component behaviour', () => {
                         return testUtils.generateRandomString(5);
                     },
                 };
+            },
+            local() {
+                return true;
             },
         });
 
@@ -55,6 +74,9 @@ describe('InputDateTime component behaviour', () => {
             autocomplete: testUtils.generateRandomString(5),
         };
 
+        if (isShallow === true)
+            return shallow(<InputDateTime {...props} />);
+
         return mount(<InputDateTime {...props} />);
     }
 
@@ -71,6 +93,45 @@ describe('InputDateTime component behaviour', () => {
         componentWrapper = manyWhoMount();
         expect(globalAny.window.manywho.component.register)
         .toHaveBeenCalledWith('input-datetime', InputDateTime); 
+    });
+
+    test('The datepicker plugin gets instantiated', () => {
+        componentWrapper = manyWhoMount(false, true);
+        expect(globalAny.datetimepickerMock).toHaveBeenCalled(); 
+    });
+
+    test('An empty date will return true', () => {
+        componentWrapper = manyWhoMount();
+        const componentWrapperInstance = componentWrapper.instance();
+        expect(componentWrapperInstance.isEmptyDate(null)).toBeTruthy();
+        expect(componentWrapperInstance.isEmptyDate('01/01/0001')).toBeTruthy();
+        expect(componentWrapperInstance.isEmptyDate('1/1/0001')).toBeTruthy();
+        expect(componentWrapperInstance.isEmptyDate('0001-01-01')).toBeTruthy();
+    });
+
+    test('A valid date will return false', () => {
+        componentWrapper = manyWhoMount();
+        const componentWrapperInstance = componentWrapper.instance();
+        expect(componentWrapperInstance.isEmptyDate('07/12/1986')).toBeFalsy();
+    });
+
+    test('utility function is called to determine if the datepicker plugin should default to the current date', () => {
+        componentWrapper = manyWhoMount(false, true);
+        expect(globalAny.window.manywho.utils.isEqual).toHaveBeenCalledWith(true, 'true', true);
+    });
+
+    test('datepicker plugin uses datetime format from model attributes', () => {
+        const expectedArgs = {
+            useCurrent: false,
+            locale: 'en-us',
+            format: 'YYYY-MM-DD',
+            timeZone: 'UTC',
+        };
+
+        componentWrapper = manyWhoMount(false, false, 'YYYY-MM-DD');
+        expect(globalAny.datetimepickerMock).toHaveBeenCalledWith(
+            expect.objectContaining(expectedArgs)
+        );
     });
 
 });
