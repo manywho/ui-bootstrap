c8d0553d8a0b76bbdfbb703154e7244d094d50b1
dshoulders <dshoulders@outlook.com>
2018-07-17 12:12:40 +0100

    CORE-4500 - Fix Typescript issue

--- js/createClassPatch.ts
+++ js/createClassPatch.ts
@@ -1,10 +1,8 @@
 const createReactClass = require('create-react-class');
 
-declare global {
-    interface Window { React: any; }
-}
+declare const React: any;
 
-window.React.createClass = (args) => {
+React.createClass = (args) => {
     console.error('React.createClass is no longer supported. ' +
         'This method is deprecated and has been removed from React. ' +
         'The method has been patched but this may cause unexpected results. ' +
