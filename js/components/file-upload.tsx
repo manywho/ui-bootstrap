e617c62a77f9e5c14277a05b165d738ddb52e6bd
TomFullalove <tom_fullalove@dell.com>
2019-04-30 16:00:24 +0100

    FUI-249 - Made a basic wrapper for the FileUpload component

--- js/components/file-upload.tsx
+++ js/components/file-upload.tsx
@@ -6,25 +6,14 @@ import registeredComponents from '../constants/registeredComponents';
 import IFileUploadProps from '../interfaces/IFileUploadProps';
 import outcome from './outcome';
 import '../../css/files.less';
+import FileUpload from '@boomi/react-file-upload';
 
 interface IFileStatus {
     fileName: string;
     uploadSuccessful: boolean;
 }
 
-interface IFileUploadState {
-    fileStatusList?: IFileStatus[];
-    isUploadDisabled?: boolean;
-    isFileSelected?: boolean;
-    isProgressVisible?: boolean;
-    isUploadComplete?: boolean;
-    fileNames?: string[];
-    progress?: number;
-    error?: any;
-    files?: any;
-}
-
-class FileUpload extends React.Component<IFileUploadProps, IFileUploadState> {
+class FileUploadWrapper extends React.Component<IFileUploadProps> {
 
     static defaultProps = {
         uploadCaption: 'Upload',
@@ -43,141 +32,6 @@ class FileUpload extends React.Component<IFileUploadProps, IFileUploadState> {
 
     constructor(props) {
         super(props);
-
-        this.state = {
-            fileStatusList: [],
-            isUploadDisabled: false,
-            isFileSelected: false,
-            isProgressVisible: false,
-            isUploadComplete: false,
-            fileNames: [],
-            error: null,
-        };
-    }
-
-    onUpload = () => {
-        if (this.state.fileNames.length > 0) {
-            this.setState({
-                isUploadDisabled: true,
-                isProgressVisible: true,
-                progress: 0,
-                error: null,
-            });
-
-            const files = [...this.state.files];
-
-            const model = 
-                !manywho.utils.isNullOrWhitespace(this.props.id) ? 
-                manywho.model.getComponent(this.props.id, this.props.flowKey) : 
-                false;
-
-            const request = model && model.fileDataRequest
-                ? model.fileDataRequest
-                : null;
-
-            // Second param (FileData) kept for backwards compatibility
-            return this.props.upload(
-                this.props.flowKey, 
-                null, 
-                ({ lengthComputable, loaded, total }) => {
-                    if (lengthComputable) {
-                        this.setState({ 
-                            progress: parseInt((loaded / total * 100).toString(), 10),
-                        });
-                    }
-                }, 
-                files, 
-                request,
-            )
-            .done((response) => {
-
-                const newFileStatuses: IFileStatus[] = this.state.fileNames.map(
-                    fileName => ({ fileName, uploadSuccessful: true }),
-                );
-
-                const appendedFileStatusList = [...this.state.fileStatusList, ...newFileStatuses];
-
-                this.setState({
-                    fileStatusList: appendedFileStatusList,
-                    isUploadDisabled: false,
-                    isFileSelected: false,
-                    isUploadComplete: true,
-                    fileNames: [],
-                    error: null,
-                });
-
-                setTimeout(
-                    () => {
-                        this.setState({ 
-                            isUploadComplete: false, isProgressVisible: false, progress: 100,
-                        });
-                    }, 
-                    2000,
-                );
-
-                (findDOMNode(this.refs.upload) as HTMLInputElement).value = '';
-
-                if (this.props.uploadComplete) {
-                    this.props.uploadComplete(response);
-                } else if (
-                    !this.props.uploadComplete && response && 
-                    !manywho.utils.isNullOrWhitespace(this.props.id)
-                ) {
-                    const objectData = response.objectData.map((item) => {
-                        item.isSelected = true;
-                        return item;
-                    });
-
-                    manywho.state.setComponent(
-                        this.props.id, { objectData }, this.props.flowKey, true,
-                    );
-
-                    manywho.component.handleEvent(
-                        this, 
-                        manywho.model.getComponent(this.props.id, this.props.flowKey), 
-                        this.props.flowKey,
-                    );
-                }
-            })
-            .fail((response) => {
-
-                const newFileStatuses: IFileStatus[] = this.state.fileNames.map(
-                    fileName => ({ fileName, uploadSuccessful: false }),
-                );
-
-                const appendedFileStatusList = [...this.state.fileStatusList, ...newFileStatuses];
-
-                this.setState({
-                    fileStatusList: appendedFileStatusList,
-                    isUploadDisabled: false,
-                    isProgressVisible: false,
-                    progress: 0,
-                    error: response.statusText,
-                });
-            });
-        }
-    }
-
-    onDrop = (files) => {
-        if (!this.props.isDesignTime)
-            this.onFileSelected(files);
-    }
-
-    onFileSelected = (files) => {
-        if (!this.props.isDesignTime) {
-            this.setState({
-                files,
-                fileNames: Array.prototype.slice.call(files).map(file => file.name),
-                isFileSelected: true,
-            });
-
-            const model = 
-                !manywho.utils.isNullOrWhitespace(this.props.id) && 
-                manywho.model.getComponent(this.props.id, this.props.flowKey);
-
-            if (model && model.attributes && model.attributes.isAutoUpload)
-                setTimeout(this.onUpload.bind(this));
-        }
     }
 
     render() {
@@ -192,83 +46,7 @@ class FileUpload extends React.Component<IFileUploadProps, IFileUploadState> {
         if (!this.props.isChildComponent && this.props.id)
             outcomes = manywho.model.getOutcomes(this.props.id, this.props.flowKey);
 
-        const progress = (this.state.progress || 0) + '%';
-
-        let label = null;
-        let helpInfo = null;
-        let validationMessage = null;
-        let isAutoUpload = false;
-        let uploadClassName = 'btn btn-primary';
-        let inputClassName = 'form-control filenames';
-        let progressClassName = 'progress-bar';
-        let componentClassName = 'file-upload';
-
-        if (this.props.smallInputs) {
-            uploadClassName += ' btn-sm';
-            inputClassName += ' input-sm';
-        }
-
-        if (!this.props.isUploadVisible)
-            uploadClassName += ' hidden';
-
-        if (this.state.isUploadComplete)
-            progressClassName += ' progress-bar-success';
-
-        if (model) {
-            const state = manywho.state.getComponent(this.props.id, this.props.flowKey) || {};
-
-            isAutoUpload = model.attributes && model.attributes.isAutoUpload;
-
-            label = 
-                <label>
-                    {model.label}
-                    {
-                        model.isRequired ? 
-                            <span className="input-required"> *</span> : 
-                            null
-                    }
-                </label>;
-
-            validationMessage = 
-                <span className="help-block">
-                {
-                    model.validationMessage || state.validationMessage
-                }
-            </span>;
-
-            helpInfo = <span className="help-block">{model.helpInfo}</span>;
-
-            if (model.isVisible === false)
-                componentClassName += ' hidden';
-
-            if (model.isValid === false || state.isValid === false)
-                componentClassName += ' has-error';
-        }
-
-        if (this.state.error) {
-            validationMessage = <span className="has-error"><span className="help-block">{this.state.error}</span></span>;
-        }
-
-        const dropzoneProps: any = {
-            ref: 'upload',
-            multiple: manywho.utils.isNullOrUndefined(this.props.multiple) ? 
-                model.isMultiSelect : 
-                this.props.multiple,
-            className: 'dropzone',
-        };
-
-        const buttonProps: any = {
-            className: uploadClassName,
-            disabled: 
-                this.state.isUploadDisabled || 
-                !this.state.isFileSelected || 
-                this.props.isDesignTime,
-        };
-
-        if (!this.props.isDesignTime) {
-            dropzoneProps.onDrop = this.onDrop;
-            buttonProps.onClick = this.onUpload;
-        }
+        //connect ondrop and on upload to the dropzone
 
         const Outcome : typeof outcome = manywho.component.getByName(registeredComponents.OUTCOME); 
 
@@ -276,54 +54,40 @@ class FileUpload extends React.Component<IFileUploadProps, IFileUploadState> {
             <Outcome id={outcome.id} flowKey={this.props.flowKey} />,
         );
 
-        const hintMessage = model.hintValue === ''
-            ? manywho.settings.global('localization.fileUploadMessage', this.props.flowKey)
-            : model.hintValue;
-
-        return <div className={componentClassName} id={this.props.id}>
-            <div className="clearfix">
-                {label}
-                <Dropzone {...dropzoneProps}>
-                    <div>{ hintMessage }</div>
-                </Dropzone>
-                <div className={'input-group ' + ((isAutoUpload) ? 'hidden' : '')}>
-                    <input type="text" 
-                        className={inputClassName} 
-                        readOnly={true} 
-                        value={this.state.fileNames.join(' ')} />
-                    <span className="input-group-btn">
-                        <button {...buttonProps}>{this.props.uploadCaption}</button>
-                    </span>
-                </div>
-            </div>
-            <div className={
-                'progress files-progress ' + ((this.state.isProgressVisible) ? '' : 'hidden')
-                }>
-                <div className={progressClassName} style={{ width: progress }} />
-            </div>
-            {validationMessage}
-            <ul className="file-statuses">
-                {
-                    this.state.fileStatusList.map((fileStatus, index) => (
-                        <li key={index} className={fileStatus.uploadSuccessful ? 'has-success' : 'has-error'}>
-                            <span className="help-block">
-                                <span className={`glyphicon glyphicon-${fileStatus.uploadSuccessful ? 'ok' : 'remove'}`}>
-                                </span>
-                                    { fileStatus.fileName }
-                                </span>
-                        </li>
-                    ))
-                }
-            </ul>
-            {helpInfo}
+        return <React.Fragment>
+            <FileUpload
+                multiple={manywho.utils.isNullOrUndefined(this.props.multiple) ? model.isMultiSelect : this.props.multiple}
+                upload={this.onUpload}
+                uploadCaption={this.props.uploadCaption}
+                uploadComplete={this.props.uploadComplete}
+                smallInputs={this.props.smallInputs}
+                isUploadVisible={this.props.isUploadVisible}
+                completedUpload={this.props.uploadComplete}
+                getFileUploadMessage={manywho.settings.global('localization.fileUploadMessage')}
+                loggingFunction={manywho.log.info}
+                handleEvent={() => manywho.component.handleEvent(
+                    this, 
+                    manywho.model.getComponent(this.props.id, this.props.flowKey), 
+                    this.props.flowKey,
+                )}
+                isAutoUpload={model.attributes ? model.attributes.isAutoUpload : false}
+                label={model.label}
+                isRequired={model.isRequired}
+                validationMessage={model.validationMessage}
+                isVisible={model.isVisible}
+                isValid={model.isValid}
+                hintValue={model.hintValue}
+                helpInfo={model.helpInfo}
+                disabled={this.props.isDesignTime}
+            />
             {outcomeButtons}
-        </div>;
+        </React.Fragment>;
     }
 
 }
 
-manywho.component.register(registeredComponents.FILE_UPLOAD, FileUpload, ['file_upload']);
+manywho.component.register(registeredComponents.FILE_UPLOAD, FileUploadWrapper, ['file_upload']);
 
-export const getFileUpload = () : typeof FileUpload => manywho.component.getByName(registeredComponents.FILE_UPLOAD);
+export const getFileUpload = () : typeof FileUploadWrapper => manywho.component.getByName(registeredComponents.FILE_UPLOAD);
 
-export default FileUpload;
+export default FileUploadWrapper;
