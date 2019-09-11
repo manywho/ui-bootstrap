5de572eaec4201f94206def1e0b2f97412d6ac37
dshoulders <dshoulders@outlook.com>
2019-09-11 09:33:00 +0100

    custom-theming - Add functionality for applying custom stylesheets with @ symbol prefix

--- js/services/theming.ts
+++ js/services/theming.ts
@@ -39,11 +39,18 @@ manywho.theming = (function (manywho) {
     }
 
     return {
-        apply(name) {
+        apply(name, tenantId) {
             if (themes !== null && name && themes.indexOf(name) !== -1) {
                 manywho.log.info('Switching theme to: ' + name);
                 loadTheme(manywho.cdnUrl + manywho.settings.theme('url') + '/mw-' + name + '.css');
-            } else {
+            } else if (name.startsWith('@')) {
+                // custom theme
+                // eg. &theme=@test_styles
+                // creates url: https://manywho-assets-development.s3.amazonaws.com/b32715f9-c7c1-4ea4-b3b0-a75aa8d3e095/test_styles.css   
+                // const tenantId = 'b32715f9-c7c1-4ea4-b3b0-a75aa8d3e095';
+                loadTheme(`${manywho.cdnUrl}/${tenantId}/${name.replace('@', '')}.css`); 
+            }
+            else {
                 manywho.log.error(name + ' theme cannot be found');
             }
         },
