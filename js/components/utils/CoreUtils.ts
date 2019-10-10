97152d603a9c4ea6e5d4304d891c7dbedccdd80a
jamesbrattBoomi <james_bratt@dell.com>
2019-10-10 09:49:42 +0100

    FLOW-399 - Adding some tests and some comments

--- js/components/utils/CoreUtils.ts
+++ js/components/utils/CoreUtils.ts
@@ -3,22 +3,27 @@
  */
 
 import { append, prepend } from 'ramda';
+import IOutcomeMetaData from '../../interfaces/IOutcomeMetaData';
 
 /* eslint-disable import/prefer-default-export */
 
 interface IRenderOutcomesInOrder {
-    (element: any, outcomes : any, outcomeMeta : any);
+    (element: JSX.Element, outcomes : JSX.Element[], outcomeMeta : Array<IOutcomeMetaData>);
 }
 
 /**
- * @param element
- * @param outcomes
- * @param outcomeMeta
- * @description
- * @returns JSX
+ * @param element A JSX snippet describing the component to be rendered
+ * @param outcomes An array of outcome components to be displayed inline
+ * @param outcomeMeta An array of objects, each describing an outcome
+ *
+ * @description Determines whether to render a just the UI component or the component with an inline outcome
+ *
+ * @returns Either the JSX for the component being rendered, or if there is
+ * an inline outcome to be displayed, an array of JSX snippets (one being the outcome, the other the component)
+ * the order of which depends on the isBulkAction key value in the outcome metadata
  */
 export const renderOutcomesInOrder: IRenderOutcomesInOrder = (element, outcomes, outcomeMeta) => {
-    if (!outcomes || outcomes.length === 0) {
+    if ((!outcomes || outcomes.length === 0) || (!outcomeMeta || outcomeMeta.length === 0)) {
         return element;
     }
 
---
+++ js/interfaces/IOutcomeMetaData.ts
@@ -0,0 +1,10 @@
+/**
+ * Describes a single object representing an outcome
+ * @todo Need to add the additional properties
+ */
+
+interface IOutcomeMetaData {
+    isBulkAction?: boolean;
+}
+
+export default IOutcomeMetaData;
