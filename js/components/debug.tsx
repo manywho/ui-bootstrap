f40da7880400259ff35b360e4aeaa21c053dd87a
jamesbrattBoomi <james_bratt@dell.com>
2020-03-05 12:29:01 +0000

    FLOW-716 - Fixing linting errors

--- js/components/debug.tsx
+++ js/components/debug.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import registeredComponents from '../constants/registeredComponents';
 import IComponentProps from '../interfaces/IComponentProps';
+
 import '../../css/debug.less';
 
 interface IDebugViewerState {
@@ -27,36 +28,10 @@ class DebugViewer extends React.Component<IComponentProps, IDebugViewerState> {
         this.renderLogEntries = this.renderLogEntries.bind(this);
     }
 
-    toggleValue(e) {
-
-        e.stopPropagation();
-
-        const toggle = this.state.toggle;
-        const valueElementId = e.currentTarget.getAttribute('data-value-id');
-
-        toggle[valueElementId] = !toggle[valueElementId];
-
-        this.setState({
-            toggle,
-        });
-    }
-
-    toggleHeader(e) {
-
-        const toggle = this.state.toggle;
-        toggle[e.currentTarget.id] = !toggle[e.currentTarget.id];
-
-        this.setState({
-            toggle,
-        });
-    }
-
     onBreadcrumbClick(e) {
-
-        e.preventDefault();
         e.stopPropagation();
 
-        const paths = this.state.paths;
+        const { paths } = this.state;
         const valueElementId = e.currentTarget.getAttribute('data-value-id');
         const breadcrumbs =
             Array.prototype.slice.call(e.currentTarget.parentNode.parentNode.childNodes);
@@ -71,7 +46,6 @@ class DebugViewer extends React.Component<IComponentProps, IDebugViewerState> {
                 paths[valueElementId] =
                     paths[valueElementId].split('.').slice(0, index).join('.');
             }
-
         }
 
         this.setState({
@@ -81,16 +55,37 @@ class DebugViewer extends React.Component<IComponentProps, IDebugViewerState> {
     }
 
     onValueViewClick(e) {
-
-        const paths = this.state.paths;
+        const { paths } = this.state;
         const valueElementId = e.currentTarget.getAttribute('data-value-id');
         const pathPart = e.currentTarget.getAttribute('data-path-part');
 
         paths[valueElementId] =
-            ((paths[valueElementId] || '') + '.' + pathPart).replace(/^\./gi, '');
+            (`${(paths[valueElementId] || '')} . ${pathPart}`).replace(/^\./gi, '');
 
         this.setState({
             paths,
+        });
+    }
+
+    toggleValue(e) {
+        e.stopPropagation();
+
+        const { toggle } = this.state;
+        const valueElementId = e.currentTarget.getAttribute('data-value-id');
+
+        toggle[valueElementId] = !toggle[valueElementId];
+
+        this.setState({
+            toggle,
+        });
+    }
+
+    toggleHeader(e) {
+        const { toggle } = this.state;
+        toggle[e.currentTarget.id] = !toggle[e.currentTarget.id];
+
+        this.setState({
+            toggle,
         });
     }
 
@@ -98,29 +93,29 @@ class DebugViewer extends React.Component<IComponentProps, IDebugViewerState> {
 
         const isExpanded = this.state.toggle[id] || false;
 
-        return <div className={'debug-root'} key={id}>
-            <div className={'debug-root-toggle'} id={id} onClick={this.toggleHeader}>
-                <span className={
-                    'glyphicon glyphicon-triangle-' + ((isExpanded) ? 'bottom' : 'right')
-                } />
-                <h5 className={'debug-title'}>
-                    {title}
-                </h5>
-                <span className={'label label-info'}>
-                    {values.length}
-                </span>
+        return (
+            <div className="debug-root" key={id}>
+                <div className="debug-root-toggle" id={id} onClick={this.toggleHeader} role="presentation">
+                    <span className={`glyphicon glyphicon-triangle- ${((isExpanded) ? 'bottom' : 'right')}`} />
+                    <h5 className="debug-title">
+                        {title}
+                    </h5>
+                    <span className="label label-info">
+                        {values.length}
+                    </span>
+                </div>
+                <ul className={`list-unstyled debug-values ${((isExpanded) ? '' : 'hidden')}`}>
+                    {
+                        values.map(
+                            value => this.renderValue(
+                                this.state.paths[value[idName]] || '', value, name, idName,
+                            ),
+                            this,
+                        )
+                    }
+                </ul>
             </div>
-            <ul className={'list-unstyled debug-values ' + ((isExpanded) ? '' : 'hidden')}>
-                {
-                    values.map(
-                        value => this.renderValue(
-                            this.state.paths[value[idName]] || '', value, name, idName,
-                        ),
-                        this,
-                    )
-                }
-            </ul>
-        </div>;
+        );
 
     }
 
@@ -129,123 +124,135 @@ class DebugViewer extends React.Component<IComponentProps, IDebugViewerState> {
         const isExpanded = this.state.toggle[value[idName]] || false;
         const properties = manywho.utils.getValueByPath(value, path);
 
-        const fullPath = value[name] + '.' + path;
+        const fullPath = `${value[name]} . ${path}`;
 
-        return <li className={'clearfix'} key={value[idName]}>
-            <span
-                className={'glyphicon debug-value-toggle glyphicon-triangle-' +
-                    ((isExpanded) ? 'bottom' : 'right')}
-                data-value-id={value[idName]}
-                onClick={this.toggleValue}
-            />
-            <div className={'debug-value'}>
-
-                <ol className={'breadcrumb debug-value-breadcrumb'}
+        return (
+            <li className="clearfix" key={value[idName]}>
+                <span
+                    className={`glyphicon debug-value-toggle glyphicon-triangle-'${((isExpanded) ? 'bottom' : 'right')}`}
                     data-value-id={value[idName]}
-                    onClick={this.toggleValue}>
-                    {
-                        fullPath.split('.').map(
-                            function (part) {
-                                if (!manywho.utils.isNullOrWhitespace(part)) {
+                    onClick={this.toggleValue}
+                    role="presentation"
+                />
+                <div className="debug-value">
 
-                                    return <li key={value[idName]}>
-                                        <a href={'#'}
-                                            onClick={this.onBreadcrumbClick}
-                                            data-value-id={value[idName]}>
-                                            {part}
-                                        </a>
-                                    </li>;
-                                }
-
-                                return null;
-
-                            },
-                            this,
-                        )
-                    }
-                </ol>
-                <table className={'table table-striped table-bordered debug-value-table ' +
-                    ((isExpanded) ? '' : 'hidden')}>
-                    <tbody>
+                    <ol
+                        className="breadcrumb debug-value-breadcrumb"
+                        data-value-id={value[idName]}
+                        onClick={this.toggleValue}
+                        role="presentation"
+                    >
                         {
-                            Object.keys(properties).map(
-                                function (propertyName) {
-
-                                    let propertyValue = properties[propertyName];
-                                    let propertyCaption = propertyName;
-
-                                    if (typeof propertyValue === 'object' && propertyValue) {
-
-                                        if (propertyValue.developerName)
-                                            propertyCaption = propertyValue.developerName;
-
-                                        propertyValue =
-                                            <button
-                                                className={'btn btn-primary btn-sm'}
-                                                data-value-id={value[idName]}
-                                                data-path-part={propertyName}
-                                                onClick={this.onValueViewClick}>
-                                                View
-                                            </button>;
+                            fullPath.split('.').map(
+                                (part) => {
+                                    if (!manywho.utils.isNullOrWhitespace(part)) {
+                                        return (
+                                            <li key={value[idName]}>
+                                                <button
+                                                    onClick={this.onBreadcrumbClick}
+                                                    data-value-id={value[idName]}
+                                                >
+                                                    {part}
+                                                </button>
+                                            </li>
+                                        );
                                     }
 
-                                    return <tr key={value[idName]}>
-                                        <td>{propertyCaption}</td>
-                                        <td>{propertyValue || 'null'}</td>
-                                    </tr>;
+                                    return null;
 
                                 },
                                 this,
                             )
                         }
-                    </tbody>
-                </table>
-            </div>
-        </li>;
+                    </ol>
+                    <table className={`table table-striped table-bordered debug-value-table ${((isExpanded) ? '' : 'hidden')}`}>
+                        <tbody>
+                            {
+                                Object.keys(properties).map(
+                                    (propertyName) => {
+
+                                        let propertyValue = properties[propertyName];
+                                        let propertyCaption = propertyName;
+
+                                        if (typeof propertyValue === 'object' && propertyValue) {
+
+                                            if (propertyValue.developerName) {
+                                                propertyCaption = propertyValue.developerName;
+                                            }
+
+                                            propertyValue = (
+                                                <button
+                                                    className="btn btn-primary btn-sm"
+                                                    data-value-id={value[idName]}
+                                                    data-path-part={propertyName}
+                                                    onClick={this.onValueViewClick}
+                                                >
+                                                    View
+                                                </button>
+                                            );
+                                        }
+
+                                        return (
+                                            <tr key={value[idName]}>
+                                                <td>{propertyCaption}</td>
+                                                <td>{propertyValue || 'null'}</td>
+                                            </tr>
+                                        );
+                                    },
+                                    this,
+                                )
+                            }
+                        </tbody>
+                    </table>
+                </div>
+            </li>
+        );
     }
 
     renderLogEntries(entries) {
+        const isExpanded = this.state.toggle.executionlog;
 
-        const isExpanded = this.state.toggle['executionlog'];
+        return (
+            <div className="debug-root" key="debug-root">
+                <div
+                    className="debug-root-toggle"
+                    id="executionlog"
+                    onClick={this.toggleHeader}
+                    role="presentation"
+                >
 
-        return <div className={'debug-root'} key={'debug-root'}>
-            <div
-                className={'debug-root-toggle'}
-                id={'executionlog'}
-                onClick={this.toggleHeader}>
+                    <span className={`glyphicon glyphicon-triangle-'${((isExpanded) ? 'bottom' : 'right')}`} />
+                    <h5 className="debug-title">Execution Log</h5>
+                    <span className="label label-info">{entries.length}</span>
+                </div>
+                <div className={isExpanded ? null : 'hidden'}>
+                    <table className="table table-striped">
+                        <tr>
+                            <th>Timestamp</th>
+                            <th>Message</th>
+                            <th>Data</th>
+                        </tr>
+                        {
+                            manywho.utils.convertToArray(entries).map((entry) => {
 
-                <span className={
-                    'glyphicon glyphicon-triangle-' + ((isExpanded) ? 'bottom' : 'right')
-                } />
-                <h5 className={'debug-title'}>Execution Log</h5>
-                <span className={'label label-info'}>{entries.length}</span>
+                                const timeStamp = new Date(entry.timestamp);
+
+                                return (
+                                    <tr key={entry.timestamp}>
+                                        <td>{timeStamp.toLocaleString()}</td>
+                                        <td>{entry.message}</td>
+                                        { /* TODO: display data */}
+                                    </tr>
+                                );
+                            })
+                        }
+                    </table>
+                </div>
             </div>
-            <div className={isExpanded ? null : 'hidden'}>
-                <table className={'table table-striped'}>
-                    <tr>
-                        <th>Timestamp</th><th>Message</th><th>Data</th>
-                    </tr>
-                    {
-                        manywho.utils.convertToArray(entries).map((entry) => {
-
-                            const timeStamp = new Date(entry.timestamp);
-
-                            return <tr key={entry.timestamp}>
-                                <td>{timeStamp.toLocaleString()}</td>
-                                <td>{entry.message}</td>
-                                { /* TODO: display data */}
-                            </tr>;
-
-                        })
-                    }
-                </table>
-            </div>
-        </div>;
-
+        );
     }
 
     render() {
-
         if (manywho.settings.isDebugEnabled(this.props.flowKey)) {
 
             manywho.log.info('Rendering Debug');
@@ -257,17 +264,14 @@ class DebugViewer extends React.Component<IComponentProps, IDebugViewerState> {
             const executionLog = manywho.model.getExecutionLog(this.props.flowKey) || {};
 
             const componentErrors = [];
-            for (const id in manywho.state.getComponents(this.props.flowKey)) {
 
+            Object.keys(manywho.state.getComponents(this.props.flowKey)).forEach((id) => {
                 if (manywho.state.getComponents(this.props.flowKey)[id].error) {
-
                     componentErrors.push(
                         manywho.state.getComponents(this.props.flowKey)[id].error,
                     );
-
                 }
-
-            }
+            });
 
             const children = [
                 this.renderValues(
@@ -287,17 +291,16 @@ class DebugViewer extends React.Component<IComponentProps, IDebugViewerState> {
                 this.renderLogEntries(executionLog.entries || []),
             ];
 
-            return <div className={'panel panel-default debug'}>
-                <div className={'panel-heading'}>
-                    <h3 className={'panel-title'}>Debug</h3>
+            return (
+                <div className="panel panel-default debug">
+                    <div className="panel-heading">
+                        <h3 className="panel-title">Debug</h3>
+                    </div>
+                    <div className="panel-body">{children}</div>
                 </div>
-                <div className={'panel-body'}>{children}</div>
-            </div>;
-
+            );
         }
-
         return null;
-
     }
 
 }
