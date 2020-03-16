1ee5b690b15a8a6b7bad8b08ad39045e66a5e1d4
jamesbrattBoomi <james_bratt@dell.com>
2020-03-16 16:49:32 +0000

    FLOW-1422 - Tests for items container search

---
+++ __tests__/fixtures/columns.test.fixture.ts
@@ -0,0 +1,44 @@
+/* eslint-disable indent */
+/* eslint-disable import/prefer-default-export */
+/* eslint-disable comma-dangle */
+/* eslint-disable quotes */
+/* eslint-disable quote-props */
+
+export const columnsFixture = [
+    {
+      "componentType": "",
+      "contentFormat": null,
+      "contentType": "ContentString",
+      "developerName": "Email",
+      "isDisplayValue": true,
+      "isEditable": false,
+      "label": "email",
+      "order": 0,
+      "typeElementPropertyId": "601ddc6a-fff4-478b-abe4-c4e65bc901c6",
+      "typeElementPropertyToDisplayId": null
+    },
+    {
+      "componentType": "",
+      "contentFormat": null,
+      "contentType": "ContentString",
+      "developerName": "Username",
+      "isDisplayValue": true,
+      "isEditable": false,
+      "label": "username",
+      "order": 1,
+      "typeElementPropertyId": "d8839b46-c43b-4435-9395-bd00491da16e",
+      "typeElementPropertyToDisplayId": null
+    },
+    {
+      "componentType": "",
+      "contentFormat": null,
+      "contentType": "ContentString",
+      "developerName": "Brand",
+      "isDisplayValue": true,
+      "isEditable": false,
+      "label": "brand",
+      "order": 2,
+      "typeElementPropertyId": "641f4870-8bf6-4cd9-9654-2aa04c542f43",
+      "typeElementPropertyToDisplayId": null
+    }
+];
