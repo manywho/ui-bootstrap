ce92eaf30b10b10de4e5766d152c386e1cf15399
Tom Fullalove <tom_fullalove@dell.com>
2020-10-13 11:16:20 +0100

    FLOW-1904 - Added tests to cover onSubmit functionality

--- __tests__/login.test.tsx
+++ __tests__/login.test.tsx
@@ -1,5 +1,3 @@
-import { str } from '../test-utils';
-
 import * as React from 'react';
 import { shallow } from 'enzyme';
 
@@ -11,26 +9,70 @@ describe('Login component behaviour', () => {
 
     const globalAny:any = global;
 
-    function manyWhoMount() {
+    const props = {
+        callback: {
+            execute: { apply: jest.fn() },
+            context: 'testContext',
+            args: [{
+                execute: null,
+                context: null,
+                args: null,
+            }],
+        },
+        loginUrl: 'testLoginUrl',
+        stateId: 'testStateId',
+        username: 'testUsername',
+        directoryName: 'testDirectoryName',
+        flowKey: 'testFlowKey',
+    };
 
-        const props = {
-            callback: {
-                execute: () => {},
-                context: {},
-                args: {},
-            },
-            loginUrl: str(5),
-            stateId: str(5),
-            username: str(5),
-            directoryName: str(5),
-        };
+    const tenantId = 'testTenantId';
+
+    const mockSuccess = 'mockSuccess';
+
+    const mockFailWithObjectJSON = {
+        responseJSON: { json: true },
+        responseText: 'responseTextError',
+    }
+
+    const mockFailWithStringJSON = {
+        responseJSON: 'responseJSONError',
+        responseText: 'responseTextError',
+    }
+
+    const mockSuccessAjax = () => ({
+        then: (thenFunction) => { thenFunction(mockSuccess); return ({ fail: () => {} }) },
+    });
+
+    const mockFailWithObjectJSONAjax = () => ({
+        then: () => ({ fail: (failFunction) => failFunction(mockFailWithObjectJSON) }),
+    });
+
+    const mockFailWithStringJSONAjax = () => ({
+        then: () => ({ fail: (failFunction) => failFunction(mockFailWithStringJSON) }),
+    });
+
+    function manyWhoMount() {
 
         globalAny.window.manywho.model.getComponent = () => ({
             attributes: {},
         });
+        globalAny.window.manywho.authorization = { setAuthenticationToken: jest.fn() };
+        globalAny.window.manywho.state.setLogin = jest.fn();
+        globalAny.window.manywho.utils.extractTenantId = jest.fn(() => tenantId);
+        globalAny.window.manywho.utils.isNullOrWhitespace = (value: string): boolean => {
+            if (typeof value === 'undefined' || value === null) {
+                return true;
+            }
+            return value.replace(/\s/g, '').length < 1;
+        };
 
         return shallow(<Login {...props} />);
     }
+
+    beforeEach(() => {
+        globalAny.window.manywho.ajax = { login: jest.fn(mockSuccessAjax) };
+    });
 
     afterEach(() => {
         componentWrapper.unmount();
@@ -45,6 +87,151 @@ describe('Login component behaviour', () => {
         componentWrapper = manyWhoMount();
         expect(globalAny.window.manywho.component.register)
         .toHaveBeenCalledWith('mw-login', Login, ['mw_login']); 
+    });
+
+    test('When OnSubmit login is called without username or password, an error is shown', () => {
+        componentWrapper = manyWhoMount();
+        
+        const username = 'test@example.com';
+        const password = 'test';
+        
+        // First test without any data
+        componentWrapper.instance().setState({
+            username: null,
+            password: null
+        });
+        
+        componentWrapper.instance().onSubmit();
+
+        expect(globalAny.window.manywho.ajax.login).not.toBeCalled();
+        expect(componentWrapper.state('usernameError')).toBe('This field is required.');
+        expect(componentWrapper.state('passwordError')).toBe('This field is required.');
+
+        componentWrapper = manyWhoMount();
+
+        // Then test without username
+        componentWrapper.instance().setState({
+            username: null,
+            password
+        });
+        
+        componentWrapper.instance().onSubmit();
+
+        expect(globalAny.window.manywho.ajax.login).not.toBeCalled();
+        expect(componentWrapper.state('usernameError')).toBe('This field is required.');
+        expect(componentWrapper.state('passwordError')).toBe("");
+
+        componentWrapper = manyWhoMount();
+
+        // Then test without password
+        componentWrapper.instance().setState({
+            username,
+            password: null
+        });
+        
+        componentWrapper.instance().onSubmit();
+
+        expect(globalAny.window.manywho.ajax.login).not.toBeCalled();
+        expect(componentWrapper.state('usernameError')).toBe("");
+        expect(componentWrapper.state('passwordError')).toBe('This field is required.');
+    });
+
+    test('When OnSubmit login succeeds, manywho state, auth and callback is updated to auth details', () => {
+        componentWrapper = manyWhoMount();
+        
+        const username = 'test@example.com';
+        const password = 'test';
+        
+        componentWrapper.instance().setState({
+            username,
+            password
+        });
+        
+        componentWrapper.instance().onSubmit();
+
+        expect(globalAny.window.manywho.ajax.login).toHaveBeenCalledWith(
+            props.loginUrl,
+            username,
+            password,
+            null,
+            null,
+            props.stateId,
+            tenantId,
+        );
+
+        expect(manywho.state.setLogin).toHaveBeenCalledWith(
+            null,
+            props.flowKey
+        );
+        expect(manywho.authorization.setAuthenticationToken).toHaveBeenCalledWith(
+            mockSuccess,
+            props.flowKey
+        );
+        expect(props.callback.execute.apply).toHaveBeenCalledWith(
+            props.callback.context,
+            [props.callback].concat(props.callback.args)
+        );
+        expect(componentWrapper.state('faults')).toBe(null);
+    });
+
+    test('When OnSubmit login fails, and the error responseJSON is a string, the string is displayed', () => {
+        globalAny.window.manywho.ajax = { login: jest.fn(mockFailWithStringJSONAjax) };
+        
+        componentWrapper = manyWhoMount();
+        
+        const username = 'test@example.com';
+        const password = 'test';
+        
+        componentWrapper.instance().setState({
+            username,
+            password
+        });
+        
+        componentWrapper.instance().onSubmit();
+
+        expect(globalAny.window.manywho.ajax.login).toHaveBeenCalledWith(
+            props.loginUrl,
+            username,
+            password,
+            null,
+            null,
+            props.stateId,
+            tenantId,
+        );
+
+        expect(componentWrapper.state('loading')).toBe(null);
+        expect(componentWrapper.state('password')).toBe('');
+        expect(componentWrapper.state('faults')).toBe(mockFailWithStringJSON.responseJSON);
+    });
+
+    test('When OnSubmit login fails, and the error responseJSON is not a string, the responseText is displayed', () => {
+        globalAny.window.manywho.ajax = { login: jest.fn(mockFailWithObjectJSONAjax) };
+        
+        componentWrapper = manyWhoMount();
+        
+        const username = 'test@example.com';
+        const password = 'test';
+        
+        componentWrapper.instance().setState({
+            username,
+            password
+        });
+        
+        componentWrapper.instance().onSubmit();
+
+        expect(globalAny.window.manywho.ajax.login).toHaveBeenCalledWith(
+            props.loginUrl,
+            username,
+            password,
+            null,
+            null,
+            props.stateId,
+            tenantId,
+        );
+
+        expect(componentWrapper.state('loading')).toBe(null);
+        expect(componentWrapper.state('password')).toBe('');
+        expect(componentWrapper.state('faults')).toBe(mockFailWithObjectJSON.responseText);
     });
 
 });
